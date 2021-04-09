d587de3694bdaf05b18f5f98dea436675615c218
HuydDo <huydinhdo@yahoo.com>
2021-04-08 22:50:38 -0600

    updated 2-counter

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -16,10 +16,19 @@ btns.forEach(function (btn) {
     else if(styles.contains('increase')){
       count++;
     }
-    else 
+    else {
       count = 0;
-      
+    }
     value.textContent = count;
+    if(count > 0){
+      value.style.color = "green"
+    }
+    if(count < 0){
+      value.style.color = "red"
+    }
+    if(count === 0){
+      value.style.color = "black"
+    }
     
   })
 })
