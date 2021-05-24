750844c9a6e8211e4d9ab643bb8a8e5822649ed2
HuydDo <huydinhdo@yahoo.com>
2021-05-24 00:21:29 -0600

    modified app.js

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -15,9 +15,16 @@ const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function (){
   // linksContainer.classList.toggle("show-links");
-  const containerHeight = linksContainer.getBoundingClientRect();
-  console.log(containerHeight)
-})
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  // console.log(linksHeight)
+  if(containerHeight === 0){
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
