5727e8a247493909c7740bb95be7f7ffbda52b69
HuydDo <huydinhdo@yahoo.com>
2021-05-26 00:22:53 -0600

    modified index, setup query selector variables and added event listener to about

---
+++ 11-tabs/setup/app.js
@@ -0,0 +1,7 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const article = document.querySelectorAll('.content');
+
+about.addEventListener('click', function(e){
+  console.log(e.target)
+})
