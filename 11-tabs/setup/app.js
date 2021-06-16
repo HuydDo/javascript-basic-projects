31aeb4295326f95ce0aa8b7f096205f577d51fce
HuydDo <huydinhdo@yahoo.com>
2021-06-15 23:58:54 -0600

    modified app.js

--- 11-tabs/setup/app.js
+++ 11-tabs/setup/app.js
@@ -3,5 +3,18 @@ const about = document.querySelector('.about');
 const article = document.querySelectorAll('.content');
 
 about.addEventListener('click', function(e){
-  console.log(e.target)
-})
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
