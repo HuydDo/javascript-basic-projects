d9036c7ba367a7072abf646a1cf8ab4654a9f5fe
HuydDo <huydinhdo@yahoo.com>
2021-06-20 23:34:21 -0600

    modified app.js

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,25 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4')
+console.log(items);
+
+let futureDate = new Date(2021,11,14,11,30,0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+// console.log(months[month]);
+
+let date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+console.log(weekday)
+giveaway.textContent = `giveaway ends on ${weekday}, ${month} ${date} ${year} ${hours}:${minutes}am`;
