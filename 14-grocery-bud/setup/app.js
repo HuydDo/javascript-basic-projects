13266b8f85f0d3b6ecc84b357cc9cea486a19d23
HuydDo <huydinhdo@yahoo.com>
2021-11-08 23:27:50 -0700

    added displayAlert func

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -29,9 +29,14 @@ function addItem(e){
   } else if (value !== '' && editFlag ){
     console.log('editing')
   }else {
-    alert.textContent = 'empty value'
-    alert.classList.add("alert-danger")
+    displayAlert("Please enter value", "danger")
   }
+}
+
+//display alert
+function displayAlert(text,action){
+  alert.textContent = text
+  alert.classList.add(`alert-${action}`)
 }
 // ****** LOCAL STORAGE **********
 
