74b26268c24d1b6a45917dc96d4c5848ff00f561
HuydDo <huydinhdo@yahoo.com>
2021-11-11 00:04:47 -0700

    update  delete item functions

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -93,8 +93,17 @@ function clearItems() {
 }
 
 // delete function
-function deleteItem() {
-  console.log("item deleted");
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if(list.children.length === 0){
+    container.classList.remove('show-container')
+  }
+  displayAlert('item removed', 'danger');
+  setBackToDefault();
+  // remove from local storage
+  // removeFromLocalStorage(id);
 }
 //edit function
 function editItem() {
@@ -110,6 +119,9 @@ function setBackToDefault() {
 }
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
-  console.log("added to local storage");
+  // console.log("added to local storage");
+}
+function removeFromLocalStorage(id){
+
 }
 // ****** SETUP ITEMS **********
